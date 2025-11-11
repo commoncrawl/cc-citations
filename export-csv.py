960e9cbdb7aef1dd6a15eb950572c34220e392c3
Hande Celikkanat <7702228+handecelikkanat@users.noreply.github.com>
2025-11-11 17:40:41 +0200

    feat(export-csv.py): add support for exploding keywords/cc-class fields with multiple values into multiple lines

--- export-csv.py
+++ export-csv.py
@@ -1,3 +1,21 @@
+''' Exports annotated .bib files to one big csv.
+
+    Optional flag --explode creates multiple rows per paper,
+      so that every row has one value per key.
+      This behavior is currently **hard-coded only over keywords and cc-class**
+      fields, which hold multiple values.
+
+      It adds three new fields to the csv:
+         - bib_id as unique identifier, to match papers over multiple lines
+         - an aggregate field cc-topic that combines values from cc-class
+           and keyword fields (both of which can include multiple values)
+         - a field cc-og-key that holds which original field cc-topic value
+           came from, options: keyword or cc-class
+      Accordingly, it removes fields keyword and cc-class, which are instead
+        aggregated into cc-topic field.
+
+    Usage: python export-csv.py bibfile [--explode]
+'''
 import csv
 import re
 import sys
@@ -6,6 +24,7 @@ from pybtex.database.input import bibtex
 
 csv_out = sys.stdout
 bibtex_in = sys.argv[1]
+explode = True if len(sys.argv) > 2 and '--explode' in sys.argv else False
 
 parser = bibtex.Parser()
 bibdata = parser.parse_file(bibtex_in)
@@ -14,24 +33,49 @@ clean_pattern = re.compile('(?<!\\\\)[{}]')
 unescape_pattern = re.compile('\\\\([{}%])')
 
 csvwriter = csv.writer(sys.stdout, quoting=csv.QUOTE_ALL)
-column_names = [
-    # old website csv
-    "cc_project_author",
-    "post_title",
-    "cc_project_url",
-    "cc_project_category",  # always 'papers'
-    "post_date",
-    # full csv
-    "keywords",
-    "abstract",
-    "cc_author_affiliation",  # removed from cc_project_author
-    "cc_class",
-    "cc_snippet",
-    "cc_dataset_used",
-    "cc_derived_dataset_about",
-    "cc_derived_dataset_used",
-    "cc_derived_dataset_cited",
-]
+
+if explode==False:
+    column_names = [
+        # old website csv
+        "cc_project_author",
+        "post_title",
+        "cc_project_url",
+        "cc_project_category",  # always 'papers'
+        "post_date",
+        # full csv
+        "keywords",
+        "abstract",
+        "cc_author_affiliation",  # removed from cc_project_author
+        "cc_class",
+        "cc_snippet",
+        "cc_dataset_used",
+        "cc_derived_dataset_about",
+        "cc_derived_dataset_used",
+        "cc_derived_dataset_cited",
+    ]
+else:
+    # change header for enabling multi-line per paper:
+    #   - introduce bib_id as unique identifier
+    #   - introduce new columns: cc-topic and cc-og-key
+    #   - remove og columns: keywords and cc_class
+    column_names = [
+        "bib_id",
+        "cc_project_author",
+        "post_title",
+        "cc_project_url",
+        "cc_project_category",  # always 'papers'
+        "post_date",
+        "cc-og-key",
+        "cc-topic",
+        "abstract",
+        "cc_author_affiliation",  # removed from cc_project_author
+        "cc_class",
+        "cc_snippet",
+        "cc_dataset_used",
+        "cc_derived_dataset_about",
+        "cc_derived_dataset_used",
+        "cc_derived_dataset_cited",
+    ]
 csvwriter.writerow(column_names)
 
 for bib_id in bibdata.entries:
@@ -94,16 +138,54 @@ for bib_id in bibdata.entries:
     title = re.sub(clean_pattern, '', title)
     title = re.sub(unescape_pattern, '\1', title)
 
-    row = [
-        authorstr, title, url, 'papers', '{}0101Z00:00:00'.format(year),
-        keywords,
-        abstract,
-        cc_author_affiliation,
-        cc_class,
-        cc_snippet,
-        cc_dataset_used,
-        cc_derived_dataset_about,
-        cc_derived_dataset_used,
-        cc_derived_dataset_cited,
-    ]
-    csvwriter.writerow(row)
+    if not explode: # one csv line per paper
+        row = [
+            authorstr, title, url, 'papers', '{}0101Z00:00:00'.format(year),
+            keywords,
+            abstract,
+            cc_author_affiliation,
+            cc_class,
+            cc_snippet,
+            cc_dataset_used,
+            cc_derived_dataset_about,
+            cc_derived_dataset_used,
+            cc_derived_dataset_cited,
+        ]
+        csvwriter.writerow(row)
+
+    if explode:
+        row_template = [
+            bib_id,
+            authorstr, title, url, 'papers', '{}0101Z00:00:00'.format(year),
+            "{CC_OG_FIELD}", # placeholder
+            "{CC_TOPIC}",    # placeholder
+            abstract,
+            cc_author_affiliation,
+            cc_snippet,
+            cc_dataset_used,
+            cc_derived_dataset_about,
+            cc_derived_dataset_used,
+            cc_derived_dataset_cited,
+        ]
+
+        idx_cc_og_field = row_template.index("{CC_OG_FIELD}")
+        idx_cc_topic = row_template.index("{CC_TOPIC}")
+
+        if keywords:
+            print(keywords)
+            for keyword in keywords.split(','):
+                row = row_template.copy()
+                row[idx_cc_og_field], row[idx_cc_topic] = "keyword", keyword.strip()
+                csvwriter.writerow(row)
+        if cc_class:
+            for cc_class_instance in cc_class.split(','):
+                row = row_template.copy()
+                row[idx_cc_og_field], row[idx_cc_topic] = "cc_class", cc_class_instance.strip()
+                csvwriter.writerow(row)
+        if not keywords and not cc_class: # dump single line with both fields empty
+            row = row_template.copy()
+            row[idx_cc_og_field], row[idx_cc_topic] = "", ""
+            csvwriter.writerow(row)
+
+
+
